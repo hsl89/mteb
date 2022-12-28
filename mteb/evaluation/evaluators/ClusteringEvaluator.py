88b60d0372cbe007c069500c39da00da63b039ac
Hongshan Li <hongshal@amazon.com>
2022-12-28 06:33:32 +0000

    add rank into to ClusterinvEvaluator

--- mteb/evaluation/evaluators/ClusteringEvaluator.py
+++ mteb/evaluation/evaluators/ClusteringEvaluator.py
@@ -11,11 +11,12 @@ from .Evaluator import Evaluator
 
 
 class ClusteringEvaluator(Evaluator):
-    def __init__(self, sentences, labels, clustering_batch_size=500, limit=None, **kwargs):
+    def __init__(self, sentences, labels, rank,  clustering_batch_size=500, limit=None, **kwargs):
         super().__init__(**kwargs)
         if limit is not None:
             sentences = sentences[:limit]
             labels = labels[:limit]
+        self.rank = rank
         self.sentences = sentences
         self.labels = labels
         self.clustering_batch_size = clustering_batch_size
@@ -24,14 +25,16 @@ class ClusteringEvaluator(Evaluator):
         logger.info(f"Encoding {len(self.sentences)} sentences...")
         corpus_embeddings = np.asarray(model.encode(self.sentences))
 
-        logger.info("Fitting Mini-Batch K-Means model...")
-        clustering_model = sklearn.cluster.MiniBatchKMeans(
-            n_clusters=len(set(self.labels)), batch_size=self.clustering_batch_size
-        )
-        clustering_model.fit(corpus_embeddings)
-        cluster_assignment = clustering_model.labels_
+        if self.rank == 0:
+            logger.info("Fitting Mini-Batch K-Means model...")
+            clustering_model = sklearn.cluster.MiniBatchKMeans(
+                n_clusters=len(set(self.labels)), batch_size=self.clustering_batch_size
+            )
+            clustering_model.fit(corpus_embeddings)
+            cluster_assignment = clustering_model.labels_
 
-        logger.info("Evaluating...")
-        v_measure = sklearn.metrics.cluster.v_measure_score(self.labels, cluster_assignment)
-
-        return {"v_measure": v_measure}
+            logger.info("Evaluating...")
+            v_measure = sklearn.metrics.cluster.v_measure_score(self.labels, cluster_assignment)
+            return {"v_measure": v_measure}
+        else:
+            return {"v_measure": 0.0}
